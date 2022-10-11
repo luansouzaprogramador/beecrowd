d09431df01317b3dffd61b2d7f0fe105a8587faa
Luan Marcelino de Souza <109920817+luansouzaprogramador@users.noreply.github.com>
2022-10-11 07:30:52 -0300

    Add files via upload

---
+++ uri1000.c
@@ -0,0 +1,9 @@
+// Autor: Luan Marcelino de Souza
+// Universidade: Centro Federal de Educacao Tecnologica de Minas Gerais
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf("Hello World!\n");
+}
